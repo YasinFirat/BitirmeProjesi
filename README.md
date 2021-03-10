5426991510c2fbb67657036bb38b589edd6e2d84
YasinFirat <yasin_firat0233@hotmail.com>
2021-03-10 04:34:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BitirmeProjesi
+ Oyun Yapılacak
---
+++ android/src/com/pixempires/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pixempires.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pixempires.game.PixEmpires;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PixEmpires(), config);
+	}
+}
