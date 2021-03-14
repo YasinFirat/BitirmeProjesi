60dc2f1c682db9e89a330e2592827e05267f0b36
YasinFirat <yasin_firat0233@hotmail.com>
2021-03-15 02:52:22 +0300

    Sprite ve Animasyon eklendi.
    Görüntünün nasıl olacağına dair izlenim için sprite karakter ve animasyon eklendi ve bunların scripti yazıldı.

--- core/src/com/pixempires/game/sprites/SpriteLoader.java
+++ core/src/com/pixempires/game/sprites/SpriteLoader.java
@@ -7,4 +7,48 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * Islevi daha sonra belirlenecek.
  */
 public class SpriteLoader {
+    private Texture texture_of_sprite;
+    private TextureRegion[] sprite;
+    private int frameCount;
+    private float maxFrameTime;
+    private float currentFrameTime;
+    private int frame;
+
+
+    public SpriteLoader(Texture texture_of_sprit){
+        this.texture_of_sprite=texture_of_sprit;
+    }
+    public void update(float delta){
+        currentFrameTime+=delta;
+        if(currentFrameTime > maxFrameTime){
+            frame++;
+            currentFrameTime = 0;
+        }
+        if(frame >= frameCount)
+            frame = 0;
+    }
+
+    public SpriteLoader Split1D(int begin_x,int begin_y,int width,int height,int frameCount,int space_x_frame){
+        this.frameCount=frameCount;
+        sprite=new TextureRegion[frameCount];
+        maxFrameTime=.5f/frameCount;
+        for (int i=0; i<frameCount;i++){
+            sprite[i]=new TextureRegion(texture_of_sprite,begin_x,begin_y,width,height);
+            if(space_x_frame==0){
+                begin_x+=width;
+            }else{
+                begin_x=i*space_x_frame;
+            }
+        }
+        return this;
+    }
+    public SpriteLoader circleTime(float circle_time){
+        if (frameCount==0)
+            return this;
+        maxFrameTime=circle_time/frameCount;
+        return this;
+    }
+    public TextureRegion getFrame(){
+        return sprite[frame];
+    }
 }
--- core/src/com/pixempires/game/states/PlayState.java
+++ core/src/com/pixempires/game/states/PlayState.java
@@ -4,13 +4,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.pixempires.game.PixEmpires;
+import com.pixempires.game.sprites.SpriteLoader;
 
 public class PlayState extends State {
     private TextureRegion back_ground_reg;
+    private TextureRegion[] archers=new TextureRegion[4];
+    private SpriteLoader archer;
     public PlayState(StateManager state_manager) {
         super(state_manager);
         Texture back_ground=new Texture("map_1.png");
+        Texture archer_tex=new Texture("spr_ArcherAttack_strip_NoBkg.png");
         back_ground_reg=new TextureRegion(back_ground);
+        archer=new SpriteLoader(archer_tex)
+                .Split1D(0,0,128,128,14,180).circleTime(.5f);
         camera.setToOrtho(false,PixEmpires.WIDTH/2,PixEmpires.HEIGHT/2);
     }
     public void create() {
@@ -20,11 +26,12 @@ public class PlayState extends State {
         sprite_batch.setProjectionMatrix(camera.combined);
         sprite_batch.begin();
         sprite_batch.draw(back_ground_reg,0,0,PixEmpires.WIDTH/2,PixEmpires.HEIGHT/2);
-
+        sprite_batch.draw(archer.getFrame(),100,130);
+        sprite_batch.draw(archer.getFrame(),100,100);
         sprite_batch.end();
     }
     public void update(float delta) {
-
+        archer.update(delta);
     }
 
     protected void handleInput() {
